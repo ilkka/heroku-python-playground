604cfa6f29bd4c69d694fc3bfd6ece2636324ef7
Ilkka Laukkanen <ilkka.s.laukkanen@gmail.com>
2011-10-11 23:03:07 +0300

    Do some configurin'

--- app.py
+++ app.py
@@ -1,16 +1,27 @@
 import os
 import sys
 import urlparse
+import psycopg2
 from flask import Flask, request, session, g, redirect, url_for, \
         abort, render_template, flash
 
-import playground.settings
+if not os.environ.has_key('DATABASE_URL'):
+    raise Exception("No DATABASE_URL in environment")
+
+# some configuration
+DATABASE_URL = urlparse.urlparse(os.environ['DATABASE_URL'])
+DEBUG = True
+SECRET_KEY = 'development-secret'
+USERNAME = 'admin'
+PASSWORD = 'secret'
 
 app = Flask(__name__)
+app.config.from_object(__name__)
+app.config.from_envvar('CONFIG_FILE', silent=True)
 
 @app.route("/")
 def hello():
-    return "DATABASES contains {}".format(playground.settings.DATABASES)
+    return "DATABASE_URL is {}".format(app.config['DATABASE_URL'])
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
